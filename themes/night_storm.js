2c5fcd9a5c8bd35adcee015c908c9702a9a89a4a
Luciano Oliveira <luciano.oliveira@ancora.com.br>
2022-09-20 14:26:57 -0300

    feat; update package json

--- themes/night_storm.js
+++ themes/night_storm.js
@@ -1,60 +1,60 @@
 module.exports = {
-	name: 'Night Storm',
-	displayName: 'Night Storm',
-	theme: {
-		background: {
-			default: '#26264F',
-			success: '#32e0b8',
-			notice: '#f1fa8c',
-			warning: '#FEA2A4',
-			danger: '#ff6188',
-			surprise: '#ad8cfa',
-			info: '#7CE3EE'
-		},
-		foreground: {
-			default: '#888BC1',
-			success: '#26264F',
-			notice: '#26264F',
-			warning: '#26264F',
-			danger: '#26264F',
-			surprise: '#26264F',
-			info: '#26264F'
-		},
-		highlight: {
-			default: 'rgba(112, 112, 184, .5)',
-			xxs: 'rgba(112, 112, 184, 0.05)',
-			xs: 'rgba(112, 112, 184, 0.1)',
-			sm: 'rgba(112, 112, 184, 0.2)',
-			md: 'rgba(20, 20, 49, 0.4)',
-			lg: 'rgba(112, 112, 184, 0.6)',
-			xl: 'rgba(112, 112, 184, 0.8)'
-		},
-		styles: {
-			sidebar: {
-				background: {
-					default: '#1D1E42'
-				}
-			},
-			dialog: {
-				background: {
-					default: '#26264F'
-				}
-			},
-			paneHeader: {
-				background: {
-					success: '#32e0b8',
-					notice: '#f1fa8c',
-					warning: '#FEA2A4',
-					danger: '#ff6188',
-					surprise: '#ad8cfa',
-					info: '#7CE3EE'
-				}
-			},
-			transparentOverlay: {
-				background: {
-					default: 'rgba(22, 23, 51, 0.5)'
-				}
-			}
-		}
-	}
+  name: 'Night Storm',
+  displayName: 'Night Storm',
+  theme: {
+    background: {
+      default: '#26264F',
+      success: '#32e0b8',
+      notice: '#f1fa8c',
+      warning: '#FEA2A4',
+      danger: '#ff6188',
+      surprise: '#ad8cfa',
+      info: '#7CE3EE'
+    },
+    foreground: {
+      default: '#888BC1',
+      success: '#26264F',
+      notice: '#26264F',
+      warning: '#26264F',
+      danger: '#26264F',
+      surprise: '#26264F',
+      info: '#26264F'
+    },
+    highlight: {
+      default: 'rgba(112, 112, 184, .5)',
+      xxs: 'rgba(112, 112, 184, 0.05)',
+      xs: 'rgba(112, 112, 184, 0.1)',
+      sm: 'rgba(112, 112, 184, 0.2)',
+      md: 'rgba(20, 20, 49, 0.4)',
+      lg: 'rgba(112, 112, 184, 0.6)',
+      xl: 'rgba(112, 112, 184, 0.8)'
+    },
+    styles: {
+      sidebar: {
+        background: {
+          default: '#1D1E42'
+        }
+      },
+      dialog: {
+        background: {
+          default: '#26264F'
+        }
+      },
+      paneHeader: {
+        background: {
+          success: '#32e0b8',
+          notice: '#f1fa8c',
+          warning: '#FEA2A4',
+          danger: '#ff6188',
+          surprise: '#ad8cfa',
+          info: '#7CE3EE'
+        }
+      },
+      transparentOverlay: {
+        background: {
+          default: 'rgba(22, 23, 51, 0.5)'
+        }
+      }
+    }
+  }
 }
